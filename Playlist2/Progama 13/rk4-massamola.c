475a60053fd8595bd4640a013dba650a1a743878
3vinicius <amorimvinicius3@gmail.com>
2022-11-22 00:20:39 -0300

    Exercicios c

--- Playlist2/Progama 13/rk4-massamola.c
+++ Playlist2/Progama 13/rk4-massamola.c
@@ -4,7 +4,7 @@
 #include<complex.h>
 
 #define P 10000
-
+#define RAN() ((double)rand()/(double)(RAND_MAX))
 
 
 
@@ -12,8 +12,8 @@
 int main(int argc, char const *argv[])
 {
     FILE *fil1,*fil2;
-    fil1 = fopen("rk4massamola.dat","w");
-    fil2 = fopen("rk4massamola3d.dat","w");
+    fil1 = fopen("rk4massamola3dALE.dat","w");
+    fil2 = fopen("rk4massamola.dat","w");
 
 
 
@@ -21,7 +21,7 @@ int main(int argc, char const *argv[])
     int N,i,j;
     double m[P],kmola[P],x[P],v[P],k1x[P],k1v[P],k2x[P],k2v[P],k3x[P],k3v[P],k4x[P],k4v[P];
 
-    N = P/100;
+    N = 100;
 
 
 
@@ -29,7 +29,7 @@ int main(int argc, char const *argv[])
     for (i = 0; i <= N; i++)
     {
         x[i]=v[i]=0;
-        m[i]=1.;
+        m[i]=1.+2*RAN();
         kmola[i]=1.;
     }
     
@@ -45,7 +45,7 @@ int main(int argc, char const *argv[])
 
 
 
-    dt = 0.05;
+    dt = 0.04;
 
 
 
@@ -61,12 +61,36 @@ int main(int argc, char const *argv[])
         for (i = 1; i <= N; i++)
         {
             k2x[i]=dt*(v[i]+0.5*k1v[i]);
-            r1 = x[i-1]+0.5*k1x[i+1];
+            r1 = x[i+1]+0.5*k1x[i+1];
             r2 = x[i-1]+0.5*k1x[i-1];
             r3 = x[i]+0.5*k1x[i];
 
             k2v[i] = dt*(1./m[i])*(kmola[i]*r1+kmola[i-1]*r2-(kmola[i]+kmola[i-1])*r3);
         }
+
+        /* k3 */
+
+        for ( i = 1; i <= N; i++)
+        {
+            k3x[i]=dt*(v[i]+0.5*k2v[i]);
+            r1 = x[i+1]+0.5*k2x[i+1];
+            r2 = x[i-1]+0.5*k2x[i-1];
+            r3 = x[i]+0.5*k2x[i];
+            k3v[i] = dt*(1./m[i])*(kmola[i]*r1+kmola[i-1]*r2-(kmola[i]+kmola[i-1])*r3);
+        }
+        
+
+        /* k4 */
+        for ( i = 1; i <= N; i++)
+        {
+            k4x[i]=dt*(v[i]+k3v[i]);
+
+            r1 = x[i+1]+k3x[i+1];
+            r2 = x[i-1]+k3x[i-1];
+            r3 = x[i]+k3x[i];
+            k4v[i] = dt*(1./m[i])*(kmola[i]*r1+kmola[i-1]*r2-(kmola[i]+kmola[i-1])*r3);
+        }
+        
 
         for (i = 1; i <= N; i++)
         {
@@ -77,7 +101,7 @@ int main(int argc, char const *argv[])
         
         ET = 0.;
         j++;
-        if (j>5)
+        if (j>8)
         {
             for (i = 1; i <= N; i++)
             {
@@ -87,10 +111,10 @@ int main(int argc, char const *argv[])
 
                 ET+=r2;
 
-                printf(fil1,"%20.8g %20.8g  %20.8g\n",t,(double)i,r2);
+                fprintf(fil1,"%20.8g %20.8g  %20.8g\n",t,(double)i,r2);
             }
             j = 0;
-            fprintf(fil2,"%20.8g %20.8g  %20.8g\n",t,fabs(1-E0/ET));
+            fprintf(fil2,"%20.8g %20.8g\n",t,fabs(1-E0/ET));
         }
         
         
--- Playlist2/Progama 5/Magnus.c
+++ Playlist2/Progama 5/Magnus.c
@@ -8,13 +8,15 @@ int main(int argc, char const *argv[])
     double c100, c200,CA,CM,AA,BB,rho,Area,RR;
     double ww,vy0,vy,theta,g,v0,vt,x1,x0,y0,y1;
     double vx0,vx,m,k,dt,t,tm,t0;
+    char filename[1000];
 
 
     FILE *fil1;
     printf("Digite Omena\n");
     scanf("%lf", &ww);
 
-    fil1 = fopen("trajetorialW%1.2lf.dat","w");
+    sprintf(filename,"trajetorialW%1.2lf.dat",ww);
+    fil1 = fopen(filename,"w");
 
 
 
@@ -25,11 +27,11 @@ int main(int argc, char const *argv[])
     t0 = 0.;
     x0 = 0.;
     y0 = 1.5;
-    dt = 0.0001;
+    dt = 0.00001;
     g = 9.8;
     tm = 1.656;
     v0 = 35.;
-    theta = (10./180.)/M_PI;
+    theta = (10./180.)*M_PI;
     vx0 = v0*cos(theta);
     vy0 = v0*sin(theta);
     RR = 3.25/100;
@@ -38,7 +40,7 @@ int main(int argc, char const *argv[])
     rho = 1.18;
     AA = 0.5*rho*Area*CA;
     m = 58./1000.;
-    c200 = 0.02/dt;
+    c200 = 0.01/dt;
 
     
 
@@ -48,9 +50,14 @@ int main(int argc, char const *argv[])
         vt = sqrt(pow(vx0,2)+pow(vy0,2));
         CM = RR*ww/vt;
         
+        BB = 0.5*rho*Area*CM;
+
         vx = vx0+dt*(-AA*vt*vx0+BB*vt*vy0) / m;
         vy = vy0+dt*(-m*g-AA*vt*vy0-BB*vt*vx0)/m;
 
+
+        x1=x0+dt*vx0;
+        y1=y0+dt*vy0;
 
         vx0 = vx;
         vy0 = vy;
@@ -62,7 +69,8 @@ int main(int argc, char const *argv[])
         c100 +=1;
         if (c100>c200)
         {
-            if (y0>0.) fprintf(fil1,"%13.8f  %13.8f\n",x0,y0);
+            if (y0>0.) fprintf(fil1,"%13.8f  %13.8f\n",x0,y0); 
+            c100=0.;
         }
         
 
