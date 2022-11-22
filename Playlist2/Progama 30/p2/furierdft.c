f696eab02a48cd23d98ba49d740fa7025e98057e
3vinicius <amorimvinicius3@gmail.com>
2022-11-22 20:44:04 -0300

    exercicios

--- Playlist2/Progama 30/p2/furierdft.c
+++ Playlist2/Progama 30/p2/furierdft.c
@@ -7,25 +7,26 @@
 
 int main(int argc, char const *argv[])
 {
+    
     FILE *fil1,*fil2;
     fil1 = fopen("saida.dat","w");
    
 
     double r1,r2,r3,r4,f,df;
     double _Complex cc1,cc2,cc3,ic;
-    double x[300000],y[300000];
+    double x[30000],y[30000];
     int i,j,N,i10,i20;
-    char filname[100] = "niveisk4v2.dat";
 
-    fil2 = fopen(filname,"r");
+
+    fil2 = fopen("niveisk4v2.dat","r");
 
     ic= 0.+I;
-
-    while ((fscanf(fil2,"%lf %lf %lf %lf\n",&x[i], &y[i],&r1,&r2)!=EOF))
+    i =1;
+    while ( fscanf(fil2,"%lf %lf %lf %lf\n",&x[i], &y[i], &r1, &r2) != EOF )
     {
         i++;
     }
-    
+  
 
     i=i-1;
     N=i;
@@ -33,6 +34,7 @@ int main(int argc, char const *argv[])
 
     for ( f = 0.05; f <= 2.0; f+=df)
     {
+        
         cc1=0.+0*I;
         for ( i = 1; i <= N; i++)
         {
@@ -40,6 +42,7 @@ int main(int argc, char const *argv[])
         }
         r1=cabs(cc1);
         fprintf(fil1,"%20.8lf   %20.8lf\n",f,r1);
+        
     }
     
 
